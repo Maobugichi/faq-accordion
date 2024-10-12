9660a993748c8b0a5786dbff7c855e8a85b593d4
Maobugichi <immansamuel846@gmail.com>
2024-10-12 03:12:07 -0700

    adjusted auto height

--- index.js
+++ index.js
@@ -14,26 +14,15 @@ plus.forEach((item,index) => {
 })
 
 function showContent(item,index) {
-    console.log(item.src.includes("/assets/images/icon-plus.svg"))
  if (item.src.includes("/assets/images/icon-plus.svg")) {
-    console.log(container)
-    
     accordionContent[index].classList.add("block")
-    //clickedElement = item
     item.setAttribute("src",imgsrc)
-    container.classList.add("auto")
-    article.classList.add("auto")
-    main.style.height = "180vh"
     } else {
     accordionContent[index].classList.remove("block")
     item.setAttribute("src", plusrc)
     console.log(container.clientHeight)
     }
-
-    
     if (container.clientHeight <= 440) {
-        container.classList.remove("auto")
-        article.classList.remove("auto")
         main.style.height = "130vh"
     }
 }
